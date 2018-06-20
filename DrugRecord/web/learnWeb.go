652dc243df87a5d68638f43a30f8979fa3ba4954
bconn98 <brc5229@rit.edu>
2018-06-19 22:00:52 -0400

    Added some more html and CSS, want to get Login and Register on the same line but separate sides of the screen.

--- DrugRecord/web/learnWeb.go
+++ DrugRecord/web/learnWeb.go
@@ -2,27 +2,48 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"github.com/gorilla/mux"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
+func getHomeHandler(w http.ResponseWriter, r *http.Request) {
 	err := templates.ExecuteTemplate(w, "learning.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-var templates = template.Must(template.ParseFiles("learning.html"))
+func postHomeHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "learning.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getLoginHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "login.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getRegisterHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "register.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var templates = template.Must(template.ParseGlob("./templates/*.html"))
 
 func main() {
-	http.HandleFunc("/", viewHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", getHomeHandler).Methods("GET")
+	r.HandleFunc("/login", getLoginHandler).Methods("GET")
+	r.HandleFunc("/register", getRegisterHandler).Methods("GET")
+	r.HandleFunc("/", postHomeHandler).Methods("POST")
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
