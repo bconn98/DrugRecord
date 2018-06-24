b58870a5ecc1b173446f410990c8effd2999b4bf
bconn98 <brc5229@rit.edu>
2018-06-23 20:56:34 -0400

    Tests for the user file

---
+++ DrugRecord/web/utils/user_test.go
@@ -0,0 +1,48 @@
+package utils
+
+/**
+File: user_test
+Description: Tests all the functions in the user file
+@author Bryan Conn
+@date 6/23/18
+ */
+
+import "testing"
+
+/**
+Function: TestMakeUser
+Description: Tests if a user is properly made and can be found in the map of users
+ */
+func TestMakeUser(t *testing.T) {
+	MakeUser("bconn", "zoo123")
+	testUser1 := FindUser("bconn")
+	testUser2 := User{"bconn", 2157}
+	if testUser1 != testUser2 {
+		t.Error("Either make user or find user messed up")
+	}
+}
+
+/**
+Function: TestCheckPassword
+Description: Tests if the password matches by testing the capital letters vs
+lower case letters
+ */
+func TestCheckPassword(t *testing.T) {
+	testUser := FindUser("bconn")
+	if !CheckPassword(testUser, "zoo123"){
+		t.Error("Password doesn't match!")
+	} else if CheckPassword(testUser, "Zoo123") {
+		t.Error("The passwords should not be matching!")
+	}
+}
+
+/**
+Function: TestUser_GetUserName
+Description: Tests if the username of a user can be properly accessed.
+ */
+func TestUser_GetUserName(t *testing.T) {
+	testUser1 := User{"bconn", 2157}
+	if testUser1.GetUserName() != "bconn" {
+		t.Error("The username doesn't match!")
+	}
+}
