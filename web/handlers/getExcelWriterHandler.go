d2dd7bbd9b0ccb0d134544a48a81000e96bd80a7
Bryan <brc5229@rit.edu>
2020-10-26 08:09:00 -0400

    fix pipeline again

--- web/handlers/getExcelWriterHandler.go
+++ web/handlers/getExcelWriterHandler.go
@@ -8,10 +8,6 @@ package handlers
 
 import (
 	"net/http"
-
-	"github.com/sqweek/dialog"
-
-	"github.com/bconn98/DrugRecord/utils"
 )
 
 /**
@@ -19,11 +15,11 @@ Function: GetExcelWriterHandler
 Description: Executes the excel writer
 */
 func GetExcelWriterHandler(acWriter http.ResponseWriter, acRequest *http.Request) {
-	lcFileName, err := dialog.File().Filter("Excel Workbook (*.xlsx)", "xlsx").Title("Export to XLSX").Save()
-	if err != nil {
-		utils.Log(err.Error(), utils.ERROR)
-	}
-	utils.ExcelWriter(lcFileName)
+	//lcFileName, err := dialog.File().Filter("Excel Workbook (*.xlsx)", "xlsx").Title("Export to XLSX").Save()
+	//if err != nil {
+	//	utils.Log(err.Error(), utils.ERROR)
+	//}
+	//utils.ExcelWriter(lcFileName)
 
 	GetCloseHandler(acWriter, acRequest)
 }
